bc982069f8b2c9f8dc883794b72d0796f27e4030
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-10-27 22:41:32 +0200

    Simplify code

--- run.js
+++ run.js
@@ -5,14 +5,9 @@ const bbox = [ 48.16821, 16.31701, 48.17324, 16.32580 ]
 let data = fs.readFileSync('baumkataster.json')
 data = JSON.parse(data)
 
-data.features.forEach(function (tree) {
+data = data.features.filter(function (tree) {
   const coord = tree.geometry.coordinates
-
-  if (coord[0] < bbox[1] || coord[0] > bbox[3] || coord[1] < bbox[0] || coord[1] > bbox[2]) {
-    return
-  }
-
-  console.log(tree.geometry.coordinates)
+  return (coord[0] >= bbox[1] && coord[0] <= bbox[3] && coord[1] >= bbox[0] && coord[1] <= bbox[2])
 })
 
-console.log('fertig')
+console.log(data.length)
