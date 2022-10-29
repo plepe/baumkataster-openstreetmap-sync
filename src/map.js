46cf530afb7f54530390d27aaf2393605f4778c0
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-10-29 18:25:40 +0200

    In map key, write count of trees per category

--- src/map.js
+++ src/map.js
@@ -57,6 +57,7 @@ function initMap () {
 
 function initMapKey () {
   const mapKey = document.getElementById('map-key')
+  mapKey.innerHTML = ''
   for (const text in assessments) {
     const div = document.createElement('div')
     mapKey.appendChild(div)
@@ -66,9 +67,14 @@ function initMapKey () {
     svg.innerHTML = '<svg width="25" height="25"><circle cx="13" cy="13" r="' + config.treeMarker.radius + '" style="stroke-width: ' + config.treeMarker.weight + 'px; stroke: ' + assessments[text] + '; fill: none;"></svg>'
     div.appendChild(svg)
 
+    let count = null
+    if (data) {
+      count = data.features.filter(f => f.properties.assessment === text).length
+    }
+
     const span = document.createElement('span')
     span.className = 'text'
-    span.appendChild(document.createTextNode(text))
+    span.appendChild(document.createTextNode(text + (count === null ? null : ' (' + count + ')')))
     div.appendChild(span)
   }
 }
