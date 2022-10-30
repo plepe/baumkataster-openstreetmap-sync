38810a593a12d0d15071149f2c749e32e7519d63
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-10-30 08:57:15 +0100

    Assess: Improve parsing species/taxon for tree

--- assess.js
+++ assess.js
@@ -169,12 +169,22 @@ function convertBk2OSM (properties) {
   tags['tree:ref'] = properties.BAUMNUMMER
   tags['start_date'] = properties.PFLANZJAHR
 
-  const m = properties.GATTUNG_ART.match(/^(.*) \((.*)\)$/)
-  if (m) {
-    tags['species'] = m[1]
-    tags['species.de'] = m[2]
+  if (properties.GATTUNG_ART === 'Jungbaum wird gepflanzt') {
+    tags['species'] = 'none'
   } else {
-    console.error("Can't parse GATTUNG_ART", properties.GATTUNG_ART)
+    const m = properties.GATTUNG_ART.match(/^([^']+) ('(.*)' )?\((.*)\)$/)
+    if (m) {
+      tags['taxon'] = m[1]
+      tags['species'] = m[1]
+      tags['species:de'] = m[4]
+
+      if (m[2]) {
+        tags['taxon'] = m[1] + ' ' + m[3]
+        tags['taxon:cultivar'] = m[3]
+      }
+    } else {
+      console.error("Can't parse GATTUNG_ART", properties.GATTUNG_ART)
+    }
   }
 
   return tags
