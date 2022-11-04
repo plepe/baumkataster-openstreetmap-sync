25422a86335cc614cb2df417371badd3c718043e
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-11-04 13:16:01 +0100

    speciesWikidata: load several items at once

--- src/speciesWikidata.js
+++ src/speciesWikidata.js
@@ -1,5 +1,6 @@
 const cache = {}
 const loadCache = {}
+let timeout
 
 export function speciesWikidata (species, callback) {
   const m = species.match(/^(.*) x (.*)$/)
@@ -16,8 +17,16 @@ export function speciesWikidata (species, callback) {
   }
 
   loadCache[species] = [callback]
+  if (!timeout) {
+    timeout = global.setTimeout(run, 1)
+  }
+}
 
-  const query = 'SELECT ?item ?itemLabel WHERE { SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". } ?item wdt:P31 wd:Q16521. ?item wdt:P225 "' + species + '". }'
+function run () {
+  timeout = null
+  const species = Object.keys(loadCache)
+
+  const query = 'SELECT ?item ?itemLabel ?scientific WHERE { SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". } ?item wdt:P31 wd:Q16521. ?item wdt:P225 ?scientific. ' + species.map(s => '{ ?item wdt:P225 "' + s + '". }').join(' union ') + ' }'
 
   fetch('https://query.wikidata.org/sparql?query=' + encodeURIComponent(query),
     {
@@ -29,11 +38,18 @@ export function speciesWikidata (species, callback) {
     .then(req => req.json())
     .then(res => {
       const results = res.results.bindings
-      const value = results.length ? results[0].itemLabel.value : null
-      cache[species] = value
 
-      const callbacks = loadCache[species]
-      delete loadCache[species]
-      callbacks.forEach(cb => cb(null, value))
+      species.forEach(s => cache[s] = null)
+
+      results.forEach(result => {
+        const species = result.scientific.value
+        cache[species] = result.itemLabel.value
+      })
+
+      species.forEach(s => {
+        const callbacks = loadCache[s]
+        delete loadCache[s]
+        callbacks.forEach(cb => cb(null, cache[s]))
+      })
     })
 }
