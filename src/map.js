e4a99245723fdc58087662c72a5f5f6509a8dbd3
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-11-04 22:12:58 +0100

    Add basemap as optional map backgrounds

--- src/map.js
+++ src/map.js
@@ -7,11 +7,30 @@ export const map = {
 
     app.map = L.map('map')
 
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    const layers = {}
+
+    layers['OSM Mapnik'] = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxNativeZoom: 19,
       maxZoom: 25
-    }).addTo(app.map)
+    })
+
+    layers['Basemap'] = L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+      subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+      attribution: '&copy; <a href="http://basemap.at">Basemap.at</a>',
+      maxNativeZoom: 20,
+      maxZoom: 25
+    })
+
+    layers['Basemap Orthofoto'] = L.tileLayer("http://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+      subdomains : ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+      attribution: '&copy; <a href="http://basemap.at">Basemap.at</a>',
+      maxNativeZoom: 20,
+      maxZoom: 25
+    })
+
+    layers['OSM Mapnik'].addTo(app.map)
+    L.control.layers(layers).addTo(app.map)
 
     app.map.attributionControl.setPrefix('<a target="_blank" href="https://github.com/plepe/baumkataster-openstreetmap-sync">baumkataster-openstreetmap-sync</a>')
 
