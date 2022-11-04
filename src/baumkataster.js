2df1f7b891883f58f5f8fe2fc3e5379249c288c9
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2022-11-04 08:35:53 +0100

    baumkataster: load features from tiles, filter by bbox

--- src/baumkataster.js
+++ src/baumkataster.js
@@ -1,28 +1,59 @@
+import async from 'async'
+
 import { StatusMessage } from './status'
 
 let app
-let cache
+let cache = {}
+let gridSize
 
 export const baumkataster = {
   get (bbox, callback) {
-    if (cache) {
-      return callback(null, cache)
-    }
+    const bboxLonLat = [ bbox[1], bbox[0], bbox[3], bbox[2] ]
+    const tiles = bboxLonLat.map((v, i) => parseInt(v / gridSize[i % 2]))
 
     const log = new StatusMessage('Loading baumkataster ...')
-    fetch('data/baum.geojson')
-      .then(req => req.json())
-      .then(body => {
-        log.change('Loading baumkataster ... done')
-        if (body) {
-          cache = body.features
+    async.map(range(tiles[0], tiles[2] + 1), (x, doneX) =>
+      async.map(range(tiles[1], tiles[3] + 1), (y, doneY) => {
+        const tileId = x + ',' + y
+        if (tileId in cache) {
+          return doneY(null, cache[tileId])
         }
-        callback(null, cache)
+
+        fetch('data/baumkataster-tiles/' + tileId + '.geojson')
+          .then(req => req.json())
+          .then(body => {
+            log.change('Loading baumkataster ... done')
+            if (body) {
+              cache[tileId] = body.features
+            }
+            doneY(null, cache[tileId])
+          })
+      },
+      (err, list) => {
+        if (err) { return doneX(err) }
+        doneX(null, [].concat(...list))
+      }),
+    (err, list) => {
+      if (err) { return callback(err) }
+      const allFeatures = [].concat(...list)
+
+      const bboxFeatures = allFeatures.filter(feature => {
+        const coord = feature.geometry.coordinates
+        return coord[0] >= bbox[1] && coord[0] <= bbox[3] && coord[1] >= bbox[0] && coord[1] <= bbox[2]
       })
+
+      callback(null, bboxFeatures)
+    })
   },
 
   init (_app, callback) {
     app = _app
+    gridSize = app.config.gridSize || [ 0.01, 0.01 ]
     callback()
   }
+}
+
+// Source: https://stackoverflow.com/a/45355468
+function range (start, end) {
+  return new Array(end - start).fill().map((d, i) => i + start);
 }
